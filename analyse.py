9d51c14e7c81a95981269f47399f1112fb789639
Flabby Ninja <dev@flabbyninja.net>
2021-03-28 22:03:43 +0100

    Get artist info

--- analyse.py
+++ analyse.py
@@ -1,8 +1,13 @@
 import requests
 import config
 
+headers = {}
+
 # Auth URL for time-limited tokens
 AUTH_URL = 'https://accounts.spotify.com/api/token'
+
+# base URL of all Spotify API endpoints
+BASE_URL = 'https://api.spotify.com/v1/'
 
 def get_spotify_token():
   auth_response = requests.post(AUTH_URL, {
@@ -14,8 +19,19 @@ def get_spotify_token():
   access_token = auth_response_data['access_token']
   return(access_token)
 
+def get_artist_info():
+  # Soundgarden
+  artist_id = '5xUf6j4upBrXZPg6AI4MRK'
+  # pull all artists albums
+  response = requests.get(BASE_URL + 'artists/' + artist_id + '/albums', 
+                 headers=headers, 
+                 params={'include_groups': 'album', 'limit': 50})
+  artist_data = response.json()
+  return artist_data
+
 if __name__ == "__main__":
   access_token = get_spotify_token()
   headers = {
     'Authorization': 'Bearer {token}'.format(token=access_token)
-}
+  }
+  print(get_artist_info())
